7adf14e0debb946c5c1f09b85f49e1f8ac79e43c
Deepika Mahindroo <deepika.mahindroo@sourcefuse.com>
2024-05-02 12:30:30 +0530

    revert(arc-doc): remove arc-doc
    remove arc-doc
    
    GH-34

--- projects/arc-docs/src/app/docs/docs.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app-docs',
-  templateUrl: './docs.component.html',
-  styleUrls: ['./docs.component.scss'],
-})
-export class DocsComponent {}
