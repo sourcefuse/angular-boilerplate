de91f7c0110049176148da4d3d0ef9b3ad767b87
Deepika Mahindroo <deepika.mahindroo@sourcefuse.com>
2024-06-19 10:58:38 +0530

    docs(arc-docs): backend integration doc
    backend integration doc
    
    GH-26

---
+++ projects/arc-docs/src/app/constants/doc-menu.constant.ts
@@ -0,0 +1,41 @@
+export const DOCUMENTATION_MENU_ITEMS = [
+  {
+    title: 'Getting Started',
+    icon: 'book-outline',
+    children: [
+      {
+        title: 'Introduction',
+        link: '/docs/getting-started',
+      },
+    ],
+  },
+  {
+    title: 'Guide',
+    icon: 'book-outline',
+    children: [
+      {
+        title: 'Cloning Boilerplate',
+        link: '/docs/guide/clone',
+      },
+      {
+        title: 'Backend Integration',
+        link: '/docs/guide/backend-integration',
+      },
+    ],
+  },
+
+  {
+    title: 'Auth',
+    icon: 'book-outline',
+    children: [
+      {
+        title: 'Introduction',
+        link: '/docs/auth-doc/authDocIntro',
+      },
+      {
+        title: 'Installation',
+        link: '/docs/auth-doc/installation',
+      },
+    ],
+  },
+];
