4f043d87ed34401901533d0d4840183bf975c978
Deepika Mahindroo <deepika.mahindroo@sourcefuse.com>
2024-06-26 17:49:28 +0530

    fix(arc-docs): resolved issues
    resolved issues
    
    Y

--- projects/arc-docs/src/app/docs/auth-doc/auth-doc.module.ts
+++ projects/arc-docs/src/app/docs/auth-doc/auth-doc.module.ts
@@ -5,10 +5,10 @@ import {AuthDocRoutingModule} from './auth-doc-routing.module';
 import {DocIntrodutionComponent} from './components/doc-introdution/doc-introdution.component';
 import {CliWrapperComponent} from '@project-lib/components/cli-wrapper/cli-wrapper.component';
 import {ThemeModule} from '@project-lib/theme/theme.module';
-import {InstallationDocComponent} from './components/installation-doc/installation-doc.component';
 
 @NgModule({
-  declarations: [DocIntrodutionComponent, InstallationDocComponent],
+  declarations: [DocIntrodutionComponent],
+
   imports: [
     CommonModule,
     AuthDocRoutingModule,
